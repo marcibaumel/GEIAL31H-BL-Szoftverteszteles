f0a7a120985cf15f75dc3b3bac477ee5129792bf
Baumel Márton <marcibaumel@gmail.com>
2021-10-13 11:33:09 +0200

    Fixed mock

--- autoTestExample/src/test/java/user/UserTest.java
+++ autoTestExample/src/test/java/user/UserTest.java
@@ -2,17 +2,20 @@ package user;
 
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class UserTest {
 
-
+    @Mock DatabaseConnection databaseConnection;
+    @InjectMocks User u;
 
     @Test
     public void sumTest(){
@@ -33,24 +36,35 @@ public class UserTest {
         Integer expected = 9;
 
         assertEquals(sum, expected);
-
     }
-    @Mock DatabaseConnection databaseConnection;
 
     @Test
-    public void testSumWithMock(){
+    public void testSumWithMockAndCorrectData(){
 
-        User u = new User(databaseConnection);
+        u = new User(databaseConnection);
         String user = "Valaki";
         String password = "123";
 
-        //TODO MOCK
         when(databaseConnection.checkUserPass(user, password)).thenReturn(true);
 
         Integer sum = u.sumData(user, password, 5, 4);
         Integer expected = 9;
 
         assertEquals(sum, expected);
+    }
 
+    @Test
+    public void testSumWithMockAndNotData(){
+        u = new User(databaseConnection);
+        String user = "Valaki";
+        String password = "123";
+
+        //TODO MOCK
+        when(databaseConnection.checkUserPass(user, password)).thenReturn(false);
+
+        Integer sum = u.sumData(user, password, 5, 4);
+        Integer expected = null;
+
+        assertEquals(sum, expected);
     }
 }
